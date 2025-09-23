93d7c861572735db60401e14c7592e034e9a2645
Sumit Sahu <71073768+SUMIT-WEBDEV@users.noreply.github.com>
2025-09-23 11:10:29 +0530

    update

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -16,9 +16,10 @@ const Navbar: React.FC = () => {
 
                 {/* Right Actions */}
                 <div className="flex items-center space-x-2">
-                    <button className="flex items-center px-3 py-2 text-sm text-gray-600 hover:text-gray-800">
-                        <div className="w-4 h-4 mr-2 rounded-full" style={{ background: 'linear-gradient(92deg, #46B753 0.02%, #E0DC32 100.02%)' }}>
-                            <svg className="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20">
+                    <button className="flex items-center px-3 py-2 text-md text-gray-600 hover:text-gray-800">
+
+                        <div className="w-7 h-7 mr-2 rounded-full" style={{ background: 'linear-gradient(92deg, #46B753 0.02%, #E0DC32 100.02%)' }}>
+                            <svg className="w-7 h-7 text-white" fill="currentColor" viewBox="0 0 20 20">
                                 <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
                             </svg>
                         </div>

e3b02ecaeb3bfaf0df1faea3cff92858798a30b5
Rittikprasad <rittik.prasad@samta.ai>
2025-11-05 15:24:14 +0530

    changes

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -188,7 +188,7 @@ const AppRouter: React.FC = () => {
                 
                 {/* Admin Login - Public route */}
                 <Route
-                    path="/admin/signin"
+                    path="/signin/admin"
                     element={<AdminLogin />}
                 />
 

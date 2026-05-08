2b4444070cc38694213d49438a54425f0fb74aa4
Rittikprasad <rittik.prasad@samta.ai>
2026-05-08 15:42:00 +0530

    changed favicon

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,6 +4,7 @@ import { pluginReact } from '@rsbuild/plugin-react';
 export default defineConfig({
   plugins: [pluginReact()],
   html: {
-    title: 'blendedfinancetool',
+    title: 'Blended Finance Tool',
+    favicon: './src/assets/villgro-favicon.png',
   },
 });
